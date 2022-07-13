fd05497486fbe490cd33b87313d00670c049433b
lucgiovan2302 <66303592+lucgiovan2302@users.noreply.github.com>
2022-07-13 09:54:30 +0300

    Reset v-img

--- src/App.vue
+++ src/App.vue
@@ -43,7 +43,7 @@
                         </v-row>
                     </v-col>
                     <v-col lg="6" cols="6">
-                        <v-img src="https://i.postimg.cc/ZYgk7Z4S/img12.png" width="600"></v-img>
+                        <v-img src="https://i.postimg.cc/K8PFKYwN/img14.png" width="600"></v-img>
                     </v-col>
                 </v-row>
             </section>
